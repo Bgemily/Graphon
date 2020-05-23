bef25513420ea1da39f25c736d4d79b1461a9880
Bgemily <zztzhang@ucdavis.edu>
2020-05-23 12:40:50 -0700

    True version of: from density to intensity

--- graphon/simulation/functions/get_clus_degree_mat.R
+++ graphon/simulation/functions/get_clus_degree_mat.R
@@ -3,16 +3,16 @@
 get_clus_degree_mat = function(edge_time_mat, clusters, intensity=TRUE){
   degree_mat = matrix(0, nrow=length(clusters), ncol=length(clusters))
   
-  if(intensity){
-    for (q in 1:nrow(degree_mat)) {
-      for (l in 1:ncol(degree_mat)) {
-        degree_mat[q,l] = length(clusters[[q]]) * length(clustes[[l]])
-      }
-    } 
-    return(degree_mat)
-  }
-  
-  else{
+  # if(intensity){
+  #   for (q in 1:nrow(degree_mat)) {
+  #     for (l in 1:ncol(degree_mat)) {
+  #       degree_mat[q,l] = length(clusters[[q]]) * length(clustes[[l]])
+  #     }
+  #   } 
+  #   return(degree_mat)
+  # }
+  # 
+  # else{
     for (q in 1:nrow(degree_mat)) {
       for (l in 1:ncol(degree_mat)) {
         degree_mat[q,l] = sum(edge_time_mat[clusters[[q]], clusters[[l]]]<Inf)
@@ -20,7 +20,7 @@ get_clus_degree_mat = function(edge_time_mat, clusters, intensity=TRUE){
       }
     } 
     return(degree_mat)
-  }
+  # }
   
 }
 
